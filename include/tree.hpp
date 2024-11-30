c4860e9df8f7cb479cd0e0c32a499fde7bd21524
Christopher Nugent <skymage23.cn@gmail.com>
2024-11-30 13:33:58 -0600

    Continued work on generic tree infrastructure.

--- include/tree.hpp
+++ include/tree.hpp
@@ -18,41 +18,54 @@ namespace cnn_practice {
 
 
             protected:
-            TreeNode(bool is_root, std::shared_ptr<TreeNode> parent){
+            TreeNode(bool is_root, bool is_leaf, std::shared_ptr<TreeNode> parent){
                 this -> parent = parent;
                 this -> is_root = is_root;
+                this -> is_leaf = is_leaf;
             }
 
             TreeNode(TreeNode&& input){
                 this -> parent = input.parent;
                 this -> children = input.children;
+                this -> is_root = input.is_root;
+                this -> is_leaf = input.is_leaf;
             }
 
             bool is_root(){ return this -> is_root; }
+            bool is_leaf(){ return this -> is_leaf; }
 
             public:
             TreeNode() {
                 this -> parent = nullptr;
                 this -> children = nullptr;
                 this -> is_root = false;
+                this -> is_leaf = false;
             }
         };
 
         class RootTreeNode : TreeNode{
             protected:
-            RootTreeNode() : TreeNode(false, nullptr){}
+            RootTreeNode() : TreeNode(true, false, nullptr){}
         };
 
-        class LeafableTree
+        class LeafBearingTreeNode : TreeNode {
+            protected:
+            LeafBearingTreeNode(
+                bool is_leaf,
+                std::shared_ptr<TreeNode> parent
+            ) : TreeNode(false, is_leaf, parent) {}
+        };
 
-        template <typename T> class DataStoreTreeNode : TreeNode{
+        template <typename T> class DataStoreTreeNode : LeafBearingTreeNode{
             public:
             T value;
 
             protected:
-            DataStoreTreeNode(T value,
+            DataStoreTreeNode(
+                bool is_leaf,
+                T value,
                 std::shared_ptr<TreeNode> parent,
-            ) : TreeNode(parent){
+            ) : LeafBearingTreeNode(is_leaf, parent){
                 this -> value = value;
             }
 
@@ -63,6 +76,29 @@ namespace cnn_practice {
             public:
             DataStoreTreeNode() : TreeNode(){}
         };
+    };
+
+    template <typename T> class Tree : RootTreeNode {
+        protected:
+        Tree(const T& input) : RootTreeNode(){
+            this -> add(T);
+        }
+
+        //Returns a reference to the final
+        //TreeNode found when searching for
+        //"input" in the tree. If "input"
+        //is not found, returns "nullptr".
+
+        virtual std::shared_ptr<TreeNode> walk(const T& input);
+
+        public:
+        Tree() : RootTreeNode(){};
+        Tree(Tree&& input) : RootTreeNode(input){};
+
+
+        bool contains(const T& input) { return (this -> walk(input)) != nullptr; }
+        virtual void add(const T& input);
+        virtual std::vector<std::string> to_string();
     };
 };
 #endif
--- include/trie.hpp
+++ include/trie.hpp
@@ -16,69 +16,40 @@ namespace cnn_practice {
 
 
 
-class CharStringTrie : RootTreeNode {
-
-    //Nested Classes:
-    private:
-     class TrieNode : DataStoreTreeNode<char>{
-        private:
-        bool is_leaf;
-
-        TrieNode(
-            char value,
-            std::shared_ptr<TreeNode> parent,
-            bool is_leaf
-        ) : DataStoreTreeNode(value, parent){
-            this -> is_leaf = is_leaf;
-        }
-
-    };
-
-//    public:
-//     class  
-
-    //Properties:
-    private:
-    int edge_count;
-    //std::shared_ptr<TrieNode> root;
-    std::shared_ptr<
-        std::vector<
-            std::shared_ptr<TrieNode>
-        >
-    > root_children;
+class CharStringTrie : Tree<std::string> {
 
     //Methods:
+    protected:
+    TreeNode walk(const std::string&);
+
     private:
-    void insert(
-        const std::string& input,
-        std::string::const_iterator input_start,
-        std::shared_ptr<TrieNode> node_start
-    );
+    int entry_count;
 
     public:
-    CharStringTrie();
+    CharStringTrie() : Tree(){ this -> entry_count = 0; };
     CharStringTrie(const std::string input);
     CharStringTrie(CharStringTrie&& old);
-    int get_edge_count();
 
     void add(const std::string& new_input);
     bool contains(const std::string& str_to_check);
-    bool starts_with_value_in_trie(
+    bool contains(
         std::string::const_iterator begin,
         std::string::const_iterator end
     );
 
-    bool starts_with_value_in_trie(
+    bool contains(
         std::string::reverse_iterator begin,
         std::string::reverse_iterator end
     );
 
-    bool starts_with_value_in_trie(
+    bool contains(
         std::string::iterator begin,
         std::string::iterator end
     );
 
-    std::vector<std::string> dump_entries();
+    int get_entry_count() { return this -> entry_count };
+
+    std::vector<std::string> to_string();
 };
 
     };
